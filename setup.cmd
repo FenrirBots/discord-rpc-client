94b5c5f24af557093198851af5d011b52d1f34b9
FenrirBots <111782432+FenrirBots@users.noreply.github.com>
2025-08-10 02:02:57 +0100

    Updated setup script to suppress the 'where' command

--- setup.cmd
+++ setup.cmd
@@ -1,7 +1,7 @@
 echo off
 setlocal
 @REM Check if git exists on the path before continuing.
-where git.exe
+where git.exe >NUL 2>&1
 if [%errorlevel%] neq [0] (
   echo Git could not be found on the system path.
   goto :end
