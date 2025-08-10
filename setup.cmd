b5fea33330fb89e0be4c37b08cfc951ec3733700
FenrirBots <111782432+FenrirBots@users.noreply.github.com>
2025-08-10 16:07:36 +0100

    Minor change to setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,16 +1,8 @@
 echo off
 setlocal
-@REM Check if git exists on the path before continuing.
-where git.exe >NUL 2>&1
-if [%errorlevel%] neq [0] (
-  echo Git could not be found on the system path.
-  goto :end
-)
-
-@REM Just a random file to make sure we initialized our submodule's submodule.
-if not exist vendor\discord-rpc\vendor\rapidjson\readme.md (
-  git submodule update --init --recursive
-)
+cd vendor\discord-rpc
+call setup.cmd
+cd ..\..\
 :end
 endlocal
 :eof
